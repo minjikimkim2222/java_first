8afc64f346970c798394c9caf8dbd3b53d10a63e
김민지 <minji@gimminjiui-MacBookPro.local>
2023-10-02 20:03:04 +0900

    condition error edit

--- class_eclipse/java_tutorials/src/org/opentutorials/javatutorials/Condition/java_main_args.java
+++ class_eclipse/java_tutorials/src/org/opentutorials/javatutorials/Condition/java_main_args.java
@@ -1,4 +1,5 @@
 /*
+
 java에서는 보통 main(String[] args)라고 한다.
 이때, args배열은 명령줄에 전달된 인자를 저장하므로, java Login minjiki2 1111이라고 실행하면
 args[0] : "minjiki2", args[1] = "1111"이 될 것이다.
@@ -10,8 +11,12 @@ argv[0] : "./a.out", argv[1] : "minjiki2", argv[2] : "1111"이 저장된다.
 그러나, java의 "main메서드는" 매개변수로 문자열 배열을 사용한다.
 만일, java Login minjiki2 1111을 커맨드라인으로 넘기면,
 argv.length = "2"이 되고, argv[0] = "minjiki2", argv[1] = "1111"이 된다
+
 */
-public class java_test
+
+package org.opentutorials.javatutorials.Condition;
+
+public class java_main_args
 {
     public static void main(String[] args)
     {
