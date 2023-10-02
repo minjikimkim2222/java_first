ae59addec1d61eea1146b2dfb62067770064996f
김민지 <minji@gimminjiui-MacBookPro.local>
2023-10-02 14:22:17 +0900

    java

---
+++ class_eclipse/java_tutorials/src/org/opentutorials/javatutorials/Type_Conversion/Type_Conversion.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.Type_Conversion;
+
+public class Type_Conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ class_eclipse/java_tutorials/src/org/opentutorials/javatutorials/data_type/DataType_constant.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.data_type;
+
+public class DataType_constant {
+	public static void main(String[] args)
+	{
+	// 1. 실수의 표현 
+		double a = 2.2;
+		
+		float b = 2.2f;
+		float b2 = 2.2F;
+		// float형은 double과 달리, 2.2가 float이라는 걸 분명히 명시해야 함.
+		
+	// 2. 정수의 표현
+		long l = 2147483648l;
+		
+	}
+}
