506d2a6e17ad689b8f5fffce5b448668ca0f2295
김민지 <minji@gimminjiui-MacBookPro.local>
2023-10-13 15:31:33 +0900

    out of jshell

--- Planet.java
+++ Planet.java
@@ -1,7 +1,14 @@
 class Planet
-       {
-           void isItPlanet(String s)
-           {
-               System.out.printf("%s is a planet", s).println();
-           }
-       }
+{
+    public static void main(String[] args)
+    {
+        String mars = "mars";
+        isItPlanet(mars);
+    }
+    
+	static void isItPlanet(String s)
+    {
+        System.out.printf("%s is a planet", s).println();
+    }
+    
+}
